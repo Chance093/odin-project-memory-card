522d496ae8a7d10f3dbcbd34062117e630d26d5a
Chance093 <chance.dev093@gmail.com>
2023-02-14 03:29:27 -0800

    Add cards state and shuffle function
    Shuffle function will shuffle the cards state array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
+import React, { useState } from 'react';
 import styles from './App.module.css';
 
 function App() {
+	const [cards, setCards] = useState([
+		'a',
+		'b',
+		'c',
+		'd',
+		'e',
+		'f',
+		'g',
+		'h',
+		'i',
+		'j',
+		'k',
+		'l',
+	]);
+
+	function shuffle() {
+		let currentIndex = cards.length,
+			temporaryValue,
+			randomIndex;
+
+		while (0 !== currentIndex) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+			temporaryValue = cards[currentIndex];
+			cards[currentIndex] = cards[randomIndex];
+			cards[randomIndex] = temporaryValue;
+		}
+		setCards(cards);
+	}
+
 	return <div className={styles.app}>Hello World</div>;
 }
 
