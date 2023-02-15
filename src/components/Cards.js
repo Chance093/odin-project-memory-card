fff02dedebcc70a617f9e714b7e3ab0bf5c47e91
Chance093 <chance.dev093@gmail.com>
2023-02-14 19:23:01 -0800

    Update Card component to display pokemon img

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,6 +4,7 @@ function Cards({ handleClick, cards }) {
 	const list = cards.map((card) => {
 		return (
 			<div className={styles.card} key={card.id}>
+				<img src={card.sprites.front_default} alt={card.name} />
 				{card.name}
 			</div>
 		);
